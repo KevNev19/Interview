50e86ee1ccb4b6aec5107a1da398f5f7412a952f
Kevin Addison <kevin.addison3@tesco.com>
2021-12-03 10:28:55 +0000

    Updated to the latest attempt for interview

--- src/test/java/com/poker/PokerTestHandGenerator.java
+++ src/test/java/com/poker/PokerTestHandGenerator.java
@@ -1,6 +1,5 @@
 package com.poker;
 
-import com.poker.*;
 import org.junit.jupiter.params.provider.Arguments;
 
 import java.util.ArrayList;
@@ -10,80 +9,64 @@ import java.util.stream.Stream;
 public class PokerTestHandGenerator {
 
     public static Stream<Arguments> generateHighCard() {
-        List<Card> cards = new ArrayList<>();
-
-        buildHand(cards,
+        return Stream.of(Arguments.arguments(buildHand(
                 new Hearts(CardValues.ACE),
                 new Clubs(CardValues.KING),
                 new Spades(CardValues.FOUR),
                 new Hearts(CardValues.EIGHT),
-                new Diamonds(CardValues.TWO)
-        );
-
-        return Stream.of(Arguments.arguments(cards, "High Card"));
+                new Diamonds(CardValues.TWO))
+                , "High Card"));
     }
 
     public static Stream<Arguments> generate1Pair() {
-        List<Card> cards = new ArrayList<>();
-
-        buildHand(cards,
-                new Hearts(CardValues.ACE),
-                new Clubs(CardValues.ACE),
-                new Spades(CardValues.FOUR),
-                new Hearts(CardValues.SIX),
-                new Diamonds(CardValues.TWO)
-        );
-
-        return Stream.of(Arguments.arguments(cards, "1 Pair"));
+        return Stream.of(Arguments.arguments(buildHand(
+                        new Hearts(CardValues.KING),
+                        new Clubs(CardValues.KING),
+                        new Spades(CardValues.FOUR),
+                        new Hearts(CardValues.EIGHT),
+                        new Diamonds(CardValues.TWO))
+                , "1 Pair"));
     }
 
     public static Stream<Arguments> generate2Pair() {
-        List<Card> cards = new ArrayList<>();
-
-        buildHand(cards,
-                new Hearts(CardValues.ACE),
-                new Clubs(CardValues.ACE),
-                new Spades(CardValues.FOUR),
-                new Hearts(CardValues.FOUR),
-                new Diamonds(CardValues.TWO)
-        );
-
-        return Stream.of(Arguments.arguments(cards, "2 Pair"));
+        return Stream.of(Arguments.arguments(buildHand(
+                        new Hearts(CardValues.KING),
+                        new Clubs(CardValues.KING),
+                        new Spades(CardValues.FOUR),
+                        new Hearts(CardValues.FOUR),
+                        new Diamonds(CardValues.TWO))
+                , "2 Pair"));
     }
 
     public static Stream<Arguments> generate3OfAKind() {
-        List<Card> cards = new ArrayList<>();
-
-        buildHand(cards,
-                new Hearts(CardValues.ACE),
-                new Clubs(CardValues.ACE),
-                new Spades(CardValues.ACE),
-                new Hearts(CardValues.EIGHT),
-                new Diamonds(CardValues.TWO)
-        );
-
-        return Stream.of(Arguments.arguments(cards, "3 of a Kind"));
+        return Stream.of(Arguments.arguments(buildHand(
+                        new Hearts(CardValues.KING),
+                        new Clubs(CardValues.KING),
+                        new Spades(CardValues.KING),
+                        new Hearts(CardValues.EIGHT),
+                        new Diamonds(CardValues.TWO))
+                , "3 of a Kind"));
     }
 
     public static Stream<Arguments> generate4OfAKind() {
-        List<Card> cards = new ArrayList<>();
-
-        buildHand(cards,
-                new Hearts(CardValues.ACE),
-                new Clubs(CardValues.ACE),
-                new Spades(CardValues.ACE),
-                new Hearts(CardValues.ACE),
-                new Diamonds(CardValues.TWO)
-        );
-
-        return Stream.of(Arguments.arguments(cards, "4 of a Kind"));
+        return Stream.of(Arguments.arguments(buildHand(
+                        new Hearts(CardValues.KING),
+                        new Clubs(CardValues.KING),
+                        new Spades(CardValues.KING),
+                        new Hearts(CardValues.KING),
+                        new Diamonds(CardValues.TWO))
+                , "4 of a Kind"));
     }
 
-    private static void buildHand(List<Card> cards, Card card1, Card card2, Card card3, Card card4, Card card5) {
+    private static List<Card> buildHand(Card card1, Card card2, Card card3, Card card4, Card card5) {
+        List<Card> cards = new ArrayList<>();
+
         cards.add(card1);
         cards.add(card2);
         cards.add(card3);
         cards.add(card4);
         cards.add(card5);
+
+        return cards;
     }
 }
